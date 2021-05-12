173ca0db8d6b63504f7cd1f0758afe0fff4593cf
TomFaust <33514347+TomFaust@users.noreply.github.com>
2021-05-12 11:57:45 +0200

    separated orientation code into different files improved video element

--- imageClassification/js/app.js
+++ imageClassification/js/app.js
@@ -48,45 +48,6 @@ function modelLoaded(){
     // continue on your neural network journey
 }
 
-//wait for the orientation to change
-window.addEventListener("resize", function(event) {
-    orientationChanged().then(function() {
-        // Profit
-        setSize();
-      });
-    
-});
-
-// Wait until innerheight changes, for max 120 frames
-function orientationChanged() {
-    const timeout = 120;
-    return new window.Promise(function(resolve) {
-      const go = (i, height0) => {
-        window.innerHeight != height0 || i >= timeout ?
-          resolve() :
-          window.requestAnimationFrame(() => go(i + 1, height0));
-      };
-      go(0, window.innerHeight);
-    });
-  }
-
-//set the size of the body accoaring to innerheight and innerwidth
-function setSize(){
-
-    if(window.innerHeight > window.innerWidth){
-        vh = window.innerHeight * 0.01;
-        vw = window.innerWidth * 0.01;
-    }else{
-        vh = window.innerWidth * 0.01;
-        vw = window.innerHeight * 0.01;
-    } 
-
-    // Then we set the value in the --vh custom property to the root of the document 
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    document.documentElement.style.setProperty('--vw', `${vw}px`);
-    
-}
-
 //prepare the canvas
 function initSettings() {
     width = video.offsetWidth
@@ -244,15 +205,16 @@ function initializeWebcam(facingMode) {
                 }
             })
         }
-        
-    
 }
 
 document.addEventListener('visibilitychange', function(){
     if(document.visibilityState == "visible" && videoStatus == "unpaused"){
-     initializeWebcam(facingMode)
+        initializeWebcam(facingMode)
+    }else if(document.visibilityState == "hidden") {
+        webcam.getTracks().forEach(function(track) {
+            track.stop();
+        });
     }
 });
 
 initializeWebcam(facingMode)
-setSize();
