0a0f559895b3c5ffc31a969cec31e047b34830dc
joelheezen <joel_heezen96@hotmail.com>
2021-05-17 11:48:23 +0200

    fixed file-to-base64 and base64-to-file

--- js/collection.js
+++ js/collection.js
@@ -1,32 +1,64 @@
-localStorage.setItem("collection", JSON.stringify({
-	"items": [
-        {
-		"info": "lorem ipsum",
-		"years": "years to degrade",
-		"picture": "base64 maybe here? hard to save picture in json without external link."
-	},
-	{
-		"info": "lorem ipsum 2",
-		"years": "years to degrade 2",
-		"picture": "base64 maybe here? hard to save picture in json without external link. 2"
-	}
-]
-}))
+let button = document.getElementById("upload")
+let result
+let collectionArray
 
-let collectionArray = JSON.parse(localStorage.getItem("collection"))
+//puts the file through filereader and calls back the response
+function getBase64(file, onLoadCallback) {
+	var reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = onLoadCallback
+	reader.onerror = function (error) {
+	  console.log('Error: ', error);
+	};	
+}
+
+
+// button fires the function so it doesnt fire without file
+button.addEventListener("click", async function(){
+	let file = document.getElementById("input").files[0]
+	getBase64(file, function(e) {
+		//awaits result to be in the variable before moving on
+		result = e.target.result
+		setUpPage()
+	})
+	
+})
+// makes dummy json for testing purposes
+function setUpPage(){
+
+	localStorage.setItem("collection", JSON.stringify({
+		"items": [
+			{
+			"info": "lorem ipsum",
+			"years": "years to degrade",
+			"picture": result
+		},
+		{
+			"info": "lorem ipsum 2",
+			"years": "years to degrade 2",
+			"picture": result
+		}
+	]
+	}))
+	
+	collectionArray = JSON.parse(localStorage.getItem("collection"))
+
+	displayCollection()
+}
 
 
 //loops through JSON from localstorage and displays every entry right now. Can be tailored to needs.
 function displayCollection(){
     let wrapper = document.getElementById("collection-wrapper")
-    console.log(wrapper)
 
     for (i = 0; i < collectionArray.items.length; i++){
         let card = document.createElement("div")
         wrapper.appendChild(card)
-        card.innerHTML = collectionArray.items[i].info + " " + collectionArray.items[i].years + " " + collectionArray.items[i].picture
+        card.innerHTML = collectionArray.items[i].info + " " + collectionArray.items[i].years
+		// uses the base64 stored in the json to make an image
+		let image = document.createElement("img")
+		image.src = collectionArray.items[i].picture
+		wrapper.appendChild(image)
     }
 
 }
-
-displayCollection()
