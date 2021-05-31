87bbee75542b5bc43795623f53042a997318dea4
Tom Faust <tom.faust@hotmail.com>
2021-05-31 18:58:59 +0200

    changed ajax to xmlhttprequest

--- js/camera.js
+++ js/camera.js
@@ -149,16 +149,21 @@ function saveTrash(trash){
 
     let img = snapCanvas.toDataURL();   
 
-    $.ajax({
-        url:"php/save.php",
-        method:"POST",
-        data:{img: img, trash: trash},
-        cache:false,
-        success:function(data){
-            console.log(data)
-            //document.querySelector("body").style.backgroundImage = `url(${data})`;
+
+    var http = new XMLHttpRequest();
+    var url = 'php/save.php';
+    var params = `img=${img}&trash=${trash}`;
+    http.open('POST', url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
         }
-    });
+    }
+    http.send(params);
     
 }
 
